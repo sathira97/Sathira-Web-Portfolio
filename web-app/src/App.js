d402f798c3ae94e9e58995759e0e39e2439828f2
sathira97 <sathiranimhana@gmail.com>
2023-04-23 11:16:18 +0530

    adding-pages-to-main-page

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,4 +1,9 @@
+import React from "react";
 import "./index.css"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Project from "./routes/Project"
 
 function App() {
   return (
